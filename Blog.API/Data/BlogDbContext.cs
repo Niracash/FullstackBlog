9950f9587627711bab20111af60ae12411696f73
Nirakash Sapkota <83993087+Niracash@users.noreply.github.com>
2022-11-19 00:14:13 +0100

    Add project files.

---
+++ Blog.API/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.API.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Create new table in the database if it doesnt exists
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ Blog.API/Migrations/20221118132143_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogPosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BlogTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogSummary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlHandle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Visible = table.Column<bool>(type: "bit", nullable: false),
+                    BlogAuthor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EditedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPosts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPosts");
+        }
+    }
+}
